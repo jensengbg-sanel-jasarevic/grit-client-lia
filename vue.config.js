e51dbe18077b1bc339a0523755cdfe830dc6302f
Sanel Jasarevic <saneljasar199212@gmail.com>
2021-08-20 12:38:53 +0200

    delete vue.config.js

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-module.exports = {
-    outputDir: path.resolve(path.join(__dirname, "../server/dist")),
-    devServer: {
-        proxy: 'http://localhost:5000'
-    },
-};
-
-// Docker container 'http://server:5000'
