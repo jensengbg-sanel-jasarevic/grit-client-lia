bd6f15d5986223c278ad5b8a98247adf041401e7
SJ <sj@sj.com>
2021-10-09 15:02:12 +0200

    tests

---
+++ tests/unit/Home.spec.js
@@ -0,0 +1,83 @@
+import { shallowMount, mount, createLocalVue } from '@vue/test-utils'
+import Home from '@/views/Home.vue'
+import DraftInfo from '@/components/DraftInfo.vue'
+import Vuex from 'vuex';
+
+const localVue = createLocalVue()
+localVue.use(Vuex);
+
+describe('Home.vue', () => {
+  let store;
+
+	let state;
+  let actions;
+
+  beforeEach(() => {
+		state = {
+			drafts: [
+        { id: 1, filename: "image.png" }
+      ]
+		}
+
+    actions = {
+			getDrafts: jest.fn(),
+		};
+
+  store = new Vuex.Store({
+    state,
+    actions
+    })
+})
+
+  it('should when mounted dispatch action to Vuex store', () => {
+    // Arrange
+    const wrapper = shallowMount(Home, {
+      localVue,
+      store
+    })
+
+    // Act
+    const actual = actions.getDrafts
+
+    // Assert
+    expect(actual).toHaveBeenCalled();
+  })
+
+  it('should when mounted render correct data from Vuex store state via computed property', () => {
+    // Arrange
+    const wrapper = shallowMount(Home, {
+      localVue,
+      store
+    })
+    const expected = [ { id: 1, filename: "image.png" } ]
+
+    // Act
+    const actual = wrapper.vm.drafts
+
+    // Assert
+    expect(actual).toStrictEqual(expected);
+  })
+
+  it('should when mounted have child component "DraftInfo"', () => {
+    // Arrange
+    const wrapper = mount(Home, {
+       mocks: {
+        $store: {
+         dispatch: function() { },
+         state: { 
+           drafts: [] 
+          }
+        }
+      }
+    })
+    const expected = "DraftInfo"
+
+    // Act
+    const findComponent = wrapper.findComponent(DraftInfo);
+    const actual = findComponent.selector.name
+
+    // Assert
+    expect(actual).toBe(expected)
+  })
+
+})
